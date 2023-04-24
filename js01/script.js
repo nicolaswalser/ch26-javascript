09528a91b237aab326fdfbbb2ceb8fc3a69716ec
Nicolas <walsergomez@gmail.com>
2023-04-24 13:28:03 -0600

    ci: type of objects in js

--- js01/script.js
+++ js01/script.js
@@ -22,7 +22,7 @@ console.log("Max Value: ", Number.MAX_VALUE);
 console.log("Safe Integer: ", Number.MAX_SAFE_INTEGER);
 
 // BigInt
-// * Represents a numerical value that 'number' is unable to represent if unsafe *
+// * Represents a numerical value that 'number' is unable to represent if unsafe (large) *
 console.log("MAX_SAFE_INTEGER + 1: ", Number.MAX_SAFE_INTEGER + 1);
 console.log("MAX_SAFE_INTEGER + 2: ", Number.MAX_SAFE_INTEGER + 2);
 let myBigInt = 9007199254740991n; // Add 'n' at the end of number to represent as bigInt
@@ -68,7 +68,7 @@ if (myVarNull === null) {
 // Object
 const myData = {
   // key: value;
-  firstName: "Nick",
+  firstName: "Nicolas",
   lastName: "Walser",
   age: 25,
   male: true,
@@ -78,8 +78,57 @@ const myData = {
     rap: "Eminem",
     indie: "Matt Corby",
   },
+  fullName: function fullname() {
+    // return myData.firstName + " " + myData.lastName;
+    // * not recommended because if myData changes its name, the function loses its location *
+    return this.firstName + " " + this.lastName; // better
+  },
+};
+
+const myFakeData = {
+  // key: value;
+  firstName: "Peter",
+  lastName: "Pan",
+  age: null,
+  male: "Almost",
+  favArtistByGenre: {
+    country: "Nicolas Walser",
+    rock: "Nick Walser",
+    rap: "Nico Walser",
+    indie: "Nicky Walser",
+  },
+  fullName: function fullname() {
+    return this.firstName + " " + this.lastName; // 'this.' referes to a local object it can reach, wont reach myDatas attributes
+  },
 };
 console.log("Complete Data: ", myData);
 console.log("Age: ", myData.age);
 console.log("Favorite Artists by Genre: ", myData.favArtistByGenre);
 console.log("Favorite Artists by Genre: ", myData.favArtistByGenre.indie);
+console.log("Full name: ", myData.fullName());
+
+console.log("Complete Data: ", myFakeData);
+console.log("Age: ", myFakeData.age);
+console.log("Favorite Artists by Genre: ", myFakeData.favArtistByGenre);
+console.log("Favorite Artists by Genre: ", myFakeData.favArtistByGenre.indie);
+console.log("Full name: ", myFakeData.fullName());
+
+// Array
+// Stores different types of data, its first element is index 0
+
+const songsMattCorby = [
+  "Mainies",
+  "My False",
+  "Brother",
+  "Resolution",
+  { 2009: 5, 2011: 5, 2013: 7, 2016: 11, 2018: 11, 2023: 9 },
+];
+console.log("Favorite Song: " + songsMattCorby[0]);
+console.log("Songs in 2016: " + songsMattCorby[4][2016]);
+
+console.log("Total num of elements: ", songsMattCorby.length); // property
+console.log("Contains 'Resolution': ", songsMattCorby.indexOf("Resolution")); // method
+
+// -----------------------------------------------
+
+// Data Conversion
