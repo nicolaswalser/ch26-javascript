12b6ae0dd9ecffb6c671a1d87b82ea81bccb141a
Nicolas <walsergomez@gmail.com>
2023-04-26 14:32:30 -0600

    ci: arrays and cicles

---
+++ js04/js04.js
@@ -0,0 +1,22 @@
+console.log("JS04 Session");
+// ------------- Arrays and Cicles -------------
+
+/*
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
+
+while (condition) {
+  
+}
+
+*/
+
+// Declar an Array
+const namesCh26 = ["Nick", "Mayra", "Siri", "Alex", "David"];
+
+namesCh26.push("Steve");
+
+console.log(namesCh26);
