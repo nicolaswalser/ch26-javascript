a0ff00e0c2a9a1e5b77410d1330d9230cd56787b
Nicolas <walsergomez@gmail.com>
2023-05-04 16:40:01 -0600

    ci: Api Fetch sintax and example to console

--- js07/js07-apiFetch.js
+++ js07/js07-apiFetch.js
@@ -1,1 +1,27 @@
-console.log("JS07-apiFetch");
+console.log("JS07 Api Fetch");
+
+/*
+Api Fetch helps us to access and manipulate HTTP petitions. It provides
+a logical and easy way of obtaining resources from the web in a asyc manner.
+
+Syntax:
+  Solicitud get
+
+  fetch(url)
+    .then(response => {})
+    .catch(error => {});
+*/
+
+const url = "https://fakestoreapi.com/products";
+
+const getProducts = (url) => {
+  fetch(url)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+getProducts(url);
